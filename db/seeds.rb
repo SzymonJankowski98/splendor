2ab93043f31ea663b321d3b34f27b275a9d1a4ad
SzymonJankowski98 <jankowski.szymon.98@gmail.com>
2022-05-21 18:00:56 +0200

    Fix card name

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,6 @@ CardResource.create!(card: card1, resource: diamond, amount: 1)
 card2 = Card::Regular.find_or_create_by!(name: 'card2', value: 2, level: 1, resource: diamond)
 CardResource.create!(card: card2, resource: diamond, amount: 2)
 
-card3 = Card::Regular.find_or_create_by!(name: 'card2', value: 3, level: 2, resource: ruby)
+card3 = Card::Regular.find_or_create_by!(name: 'card3', value: 3, level: 2, resource: ruby)
 CardResource.create!(card: card3, resource: ruby, amount: 3)
 CardResource.create!(card: card3, resource: diamond, amount: 2)
